52c5dfbceb0b18290c4ba84badb690dda3190ab2
Teja <manasok7@gmail.com>
2019-05-11 01:27:21 +0530

    New Branch

--- com.teja.git.simple/src/com/teja/git/simple/GitTest.java
+++ com.teja.git.simple/src/com/teja/git/simple/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git is Cool!");
+		System.out.println("New Line added for Branch Merging!!!");
 	}
 
 }
